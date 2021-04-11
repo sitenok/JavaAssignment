549e75de366b339b7af866c3c2a6afd51f6081cc
Nicole Sitenok <57601862+sitenok@users.noreply.github.com>
2021-04-11 15:30:50 +0100

    Update createtable.sql

--- database/createtable.sql
+++ database/createtable.sql
@@ -4,7 +4,10 @@ and converted from an Access database (.dbf file) to a ".sql" file which creates
 https://www.dbf2002.com/dbf-converter/convert-dbf-to-sql.html )
 
 This table contains "Art in the Park Sculpture Locations". 
-Contains 19 columns: ID, NAME (name of sculpture), DCC_REF (Dublin City Council reference), ARTWORK (name of artwork), ARTIST_NAME, ARTIST_DATE (years alive), DATE_WORK (year sculpture was made), TYPE (type of artwork-all sculptures), MEDIUM (material made of), DIMENSION (size of sculptures), INSCRIPT (present yes/no), SIGNED (yes, signed where; no), DURATION (how long sculpture was up for-all permanent), LOCATION, AREA (of Dublin), COMMISSION (funded by), OWNED_BY, EASTING (coordinate), NORTHING (coordinate).
+Contains 19 columns: ID, NAME (name of sculpture), DCC_REF (Dublin City Council reference), ARTWORK (name of artwork), ARTIST_NAME, 
+ARTIST_DATE (years alive), DATE_WORK (year sculpture was made), TYPE (type of artwork-all sculptures), MEDIUM (material made of),
+DIMENSION (size of sculptures), INSCRIPT (present yes/no), SIGNED (yes, signed where; no), DURATION (how long sculpture was up for-all permanent), 
+LOCATION, AREA (of Dublin), COMMISSION (funded by), OWNED_BY, EASTING (coordinate), NORTHING (coordinate).
 Contains 40 rows of sculptures/objects with their details under each column. Some details are blank for some of the sculputres/rows.
 */
 
