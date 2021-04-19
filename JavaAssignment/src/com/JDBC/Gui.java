8d47201cb7aa591e554f44c2b4d8d0919850731a
Nicole <c19391873@mytudublin.ie>
2021-04-19 22:42:23 +0100

    buttons

--- JavaAssignment/src/com/JDBC/Gui.java
+++ JavaAssignment/src/com/JDBC/Gui.java
@@ -1,10 +1,6 @@
 package com.JDBC;
 
 import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,50 +15,76 @@ import javax.swing.JTextField;
 public class Gui extends JFrame implements ActionListener
 {
 	//attributes
-	JButton b1, b2, b3, b4, b5;
-	JLabel label;
+	JButton enterButton, showButton;
+	JLabel inputLabel;
 	JTextField input;
 	JPanel bigPanel;
-	final static int maxGap = 20;
+	private String userInput = "hello";
 	
 	//constructor sets up the screen
 	Gui(String title)
 	{
 		super(title);
-		setSize(300,300);
+		setSize(600,400);
 		
 		//layout
 		GridLayout grid = new GridLayout(5,5);
 		bigPanel = new JPanel();
-		bigPanel.setLayout(grid);
+		bigPanel.setLayout(new FlowLayout());
 		
-		//button size
-		JButton temp = new JButton();
-		Dimension buttonSize = temp.getPreferredSize();
-		bigPanel.setPreferredSize(new Dimension((int)(buttonSize.getWidth() * 2.5)+maxGap,
-        (int)(buttonSize.getHeight() * 3.5)+maxGap * 2));
+		//adding components to panel
+		inputLabel = new JLabel("Enter a Query:");
+		bigPanel.add(inputLabel);
 		
-		//adding buttons to panel
-		b1 = new JButton("Sculpture 1");
-		b2 = new JButton("Sculpture 2");
-		b3 = new JButton("Sculpture 3");
-		b4 = new JButton("Sculpture 4");
-		b5 = new JButton("Sculpture 5");
-		bigPanel.add(b1);
-		bigPanel.add(b2);
-		bigPanel.add(b3);
-		bigPanel.add(b4);
-		bigPanel.add(b5);
+		input = new JTextField();
+		input.setColumns(50);
+		bigPanel.add(input);
+		
+		enterButton = new JButton("Enter");
+		enterButton.setToolTipText("Click this button to save your query");
+		enterButton.addActionListener(this);
+		bigPanel.add(enterButton);
+		
+		showButton = new JButton("Display Result");
+		showButton.setToolTipText("Click this button to display results of your query");
+		showButton.addActionListener(this);
+		bigPanel.add(showButton);
+		
+		add(bigPanel);
 		
 		setVisible(true);
 	}
-
+	
 	//methods
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
+		//response to enter button being pressed
+		if(e.getSource() == enterButton)
+		{
+			System.out.println("Query entered");
+			setUserInput(this.input.getText()); //get the text from JTextField
+			
+			JOptionPane.showMessageDialog(this,"Query Entered.");
+			
+		}
+		if(e.getSource() == showButton)
+		{
+			System.out.println("Display");
+			
+		}
 		
-		
+	}
+	
+	//setter getter methods
+	public String getUserInput()
+	{
+		return userInput;
+	}
+
+	public void setUserInput(String userInput) 
+	{
+		this.userInput = userInput;
 	}
 
 }
